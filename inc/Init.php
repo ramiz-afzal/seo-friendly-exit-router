7f8402ae2404484c1a6c8853634cf3725a32fbbd
Ramiz Afzal <m.ramiz.afzal@gmail.com>
2024-06-13 16:25:46 +0500

    Initial commit

---
+++ inc/Init.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace PLUGIN_NAMESPACE;
+
+if (!defined('ABSPATH')) exit;
+
+final class Init
+{
+    public static function get_services()
+    {
+        return array(
+            Core\Shortcode::class,
+            Core\AjaxHandler::class,
+            Core\WordPressHooks::class,
+            Admin\AdminPages::class,
+        );
+    }
+
+    public static function register_services()
+    {
+        foreach (self::get_services() as $class) {
+
+            $service = self::instantiate($class);
+            if (method_exists($service, 'register')) {
+                $service->register();
+            }
+        }
+    }
+
+    private static function instantiate($class)
+    {
+        return new $class();
+    }
+}
